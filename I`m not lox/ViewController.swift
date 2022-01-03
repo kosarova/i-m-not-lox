c885db22f08b0c62faec4df4ac0bf960e137ec48
Kobzarev Roman <krv2306@gmail.com>
2022-01-03 15:56:14 +0300

    first commit

---
+++ I`m not lox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I`m not lox
+//
+//  Created by Roman Kobzarev on 03.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
